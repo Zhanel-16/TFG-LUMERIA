799af662e933514a4496cb27b496f8b0becad2e1
jeje <zhanelya.rrr@gmail.com>
2026-05-31 23:14:27 +0200

    forzar utf8

--- backend/bd/bd.py
+++ backend/bd/bd.py
@@ -5,18 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def db_conn():
-    conn = mysql.connector.connect(
+    return mysql.connector.connect(
         host=os.getenv("MYSQL_HOST"),
         user=os.getenv("MYSQL_USER"),
         password=os.getenv("MYSQL_PASS"),
         database=os.getenv("MYSQL_BD"),
         charset="utf8mb4",
-        use_unicode=True,
+        use_pure=True,
         autocommit=True
     )
-
-    cursor = conn.cursor()
-    cursor.execute("SET NAMES utf8mb4 COLLATE utf8mb4_general_ci")
-    cursor.close()
-
-    return conn
